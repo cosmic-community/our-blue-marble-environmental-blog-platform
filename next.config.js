fd18518f71dc45f713f47effce87e1db75f1000f
Cosmic AI <support@cosmicjs.com>
2025-12-26 09:41:23 -0800

    Initial commit from Cosmic AI

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imgix.cosmicjs.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.cosmicjs.com',
+      },
+    ],
+  },
+  typedRoutes: false,
+}
+
+module.exports = nextConfig
